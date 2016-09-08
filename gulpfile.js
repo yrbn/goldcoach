8341db3acd7a262e027a3c5ed14bfbd4b20b2d71
Yuriy Rybin <yrbn86@gmail.com>
2016-09-08 17:59:32 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp         = require('gulp'),
+    postcss      = require('gulp-postcss'),
+    sass         = require('gulp-sass'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    browser      = require('browser-sync'),
+    iconfont     = require('gulp-iconfont'),
+    consolidate  = require('gulp-consolidate');
+
+gulp.task('sass', function () {
+  return gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browser.stream({match: '**/*.css'}));
+});
+
+// Font generator
+gulp.task("build:icons", function() {
+    return gulp.src(["./icons/*.svg"]) //path to svg icons
+      .pipe(iconfont({
+        fontName: "iconsfont", 
+        formats: ["ttf", "eot", "woff", "svg"],
+        centerHorizontally: true,
+        fixedWidth: true,
+        normalize: true
+      }))
+      .on("glyphs", (glyphs) => {
+
+        gulp.src("./icons/util/*.scss") // Template for scss files
+            .pipe(consolidate("lodash", {
+                glyphs: glyphs,
+                fontName: "iconsfont",
+                fontPath: "../fonts/"
+            }))
+            .pipe(gulp.dest("./scss/icons")); //generated scss files with classes
+      })
+      .pipe(gulp.dest("./fonts/")); //icon font destination
+});
+
+// Starts a BrowerSync instance
+gulp.task('serve', ['sass'], function(){
+  browser.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// Runs all of the above tasks and then waits for files to change
+gulp.task('default', ['serve'], function() {    
+  gulp.watch(['scss/**/*.scss'], ['sass']);  
+  gulp.watch('./**/*.html').on('change', browser.reload);
+});
